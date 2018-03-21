02f90926c02bd41406947173765bae4f6b503e32
Henrique Mattos <henrique.mattos@sap.com>
2018-03-21 15:14:35 +0100

    Added filter for vehicles in garage

--- src/components/modal.js
+++ src/components/modal.js
@@ -10,12 +10,33 @@ export default class Modal extends App {
         };
         this.hideVehiclesGarage = props.hideVehiclesGarage;
         this.locateVehicleGarage = props.locateVehicleGarage;
+        this.filterVehicles = this.filterVehicles.bind(this);
+    }
+    filterVehicles() {
+        let filteredList = []
+        if (this.input.value) {
+            this.props.vehiclesGarageList.forEach((vehicles) => {
+                if (vehicles[this.select.value].toUpperCase().includes(this.input.value.toUpperCase())) {
+                    filteredList.push(vehicles);
+                }
+            });            
+        } else {
+            filteredList = this.props.vehiclesGarageList;
+        }
+        this.setState({
+            vehiclesGarageList: filteredList
+        });
+    }
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            vehiclesGarageList: nextProps.vehiclesGarageList
+        });
     }
     render(){
         if (this.props.showVehiclesGarage) {
             let vehiclesListRow = [];
-            if (this.props.vehiclesGarageList) {
-                vehiclesListRow.push(this.props.vehiclesGarageList.map((vehicle, index) => {
+            if (this.state.vehiclesGarageList) {
+                vehiclesListRow.push(this.state.vehiclesGarageList.map((vehicle, index) => {
                     return <tr key={index}>
                         <td>{vehicle.Identificacao}</td>
                         <td>{vehicle.Descricao}</td>
@@ -32,11 +53,29 @@ export default class Modal extends App {
                         <button className="delete" aria-label="close" onClick={(event) => {this.hideVehiclesGarage(event)}}></button>
                     </header>
                     <section className="modal-card-body">
+                        <div className="field has-addons">
+                            <p className="control is-expanded">
+                                <input className="input" type="text" placeholder="Encontre um veículo" ref={ (input) => this.input = input } />
+                            </p>
+                            <p className="control">
+                                <span className="select">
+                                    <select ref={ (select) => this.select = select }>
+                                        <option value="Descricao">pela descrição</option>
+                                        <option value="Placa">pela placa</option>
+                                        <option value="Identificacao">pela ID</option>
+                                        <option value="Garagem">pela garagem</option>
+                                    </select>
+                                </span>
+                            </p>
+                            <p className="control">
+                                <a className="button is-info" href="javascript:void(null)" onClick={ this.filterVehicles }>Filtrar</a>
+                            </p>
+                        </div>
                         <table className="table is-striped is-fullwidth is-narrow is-hoverable">
                             <thead>
                                 <tr>
                                     <th nowrap="nowrap">ID</th>
-                                    <th nowrap="nowrap">Descricao</th>
+                                    <th nowrap="nowrap">Descrição</th>
                                     <th nowrap="nowrap">Placa</th>
                                     <th nowrap="nowrap">Garagem</th>
                                 </tr>
